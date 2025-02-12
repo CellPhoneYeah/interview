1b52171d0a02360e3821573cee5ab5f00fe3ee71
ellphonye <ellphonye@tencent.com>
2025-02-12 10:59:05 +0800

    leetcode check

---
+++ leetcode/isvalid.h
@@ -0,0 +1,19 @@
+#include <stack>
+#include <string>
+bool isValid(std::string s) {
+    s.substr()
+    std::stack<char> stk;
+    for(char c: s){
+        if(c == '(' || c == '[' || c == '{'){
+            stk.push(c);
+        }else if(
+            (c == ')' && (stk.size() == 0 || stk.top() != '(')) 
+        || (c == ']' && (stk.size() == 0 || stk.top() != '[')) 
+        || (c == '}' && (stk.size() == 0 || stk.top() != '{'))){
+            return false;
+        }else{
+            stk.pop();
+        }
+    }
+    return stk.size() == 0;
+}
